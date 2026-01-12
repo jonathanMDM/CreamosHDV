c268b1ba10120f2d81011978b8d425b29951af7e
jonathan montes diaz <jonathanm@MacBook-Pro-de-jonathan.local>
2026-01-11 20:11:29 -0500

    Add missing cloudinary config file to fix 500 error

---
+++ config/cloudinary.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud 
+    | service that offers a solution to a web application's entire image 
+    | management pipeline. 
+    |
+    */
+
+    'cloud_url' => env('CLOUDINARY_URL'),
+
+    /**
+    | Upload Preset
+    |
+    */
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+
+];
