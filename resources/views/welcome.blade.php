15fb6148fce9d00c7a077c163cbbf4525f724b2b
jonathan montes diaz <jonathanm@MacBook-Pro-de-jonathan.local>
2026-01-13 21:05:21 -0500

    Fix: Force cache bust for CSS and set navbar to off-white

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <!-- Custom CSS -->
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}?v=2.0" rel="stylesheet">
 
     <style>
         body {
