6cc7562663f1dff4fd37a83fbadae62e8cd5b3ff
jonathan montes diaz <jonathanm@MacBook-Pro-de-jonathan.local>
2026-01-16 17:37:46 -0500

    Design: Dark transparent navbar and mobile menu improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,10 +55,10 @@
     <nav class="navbar navbar-expand-lg landing-navbar">
         <div class="container px-4 px-lg-0">
             <a class="navbar-brand d-flex align-items-center" href="/">
-                <img src="{{ asset('images/logo.png') }}" alt="Logo" height="40" class="me-2" style="filter: brightness(0);">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo" height="40" class="me-2" style="filter: brightness(0) invert(1);">
             </a>
-            <button class="navbar-toggler border shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" style="background: rgba(255,255,255,0.8);">
-                <i class="fas fa-bars text-dark"></i>
+            <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" style="background: rgba(255,255,255,0.1);">
+                <i class="fas fa-bars text-white"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mx-auto">
